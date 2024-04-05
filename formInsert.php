e93066fcb134057574e8c7c85bc41a83b991fb1f
ClaytonM19 <116005388+ClaytonM19@users.noreply.github.com>
2024-04-05 16:34:18 -0300

    Add files via upload

---
+++ formInsert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br"> <head>
+     <meta charset="utf-8">
+     <title> Insert </title>
+</head>
+<body>  
+  <header> Cadastrar dados dos clientes </header>
+  <section>
+     <form action="insert.php" method="post">
+
+      <p> Nome: <input type="text" name="nome"> </p>
+      <p> E-mail: <input type="email" name="email"> </p>
+      <p> Idade: <input type="number" name="idade"> </p>
+
+       <button type="submit"> Inserir dados </button>
+    </form>
+ </section>
+</body>
+</html>
